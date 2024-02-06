8bc504418295c59f0dc8ffa4384d6652fe4da521
Jovan sanon <60199315+blackice416@users.noreply.github.com>
2024-02-06 15:26:27 -0500

    fourth commit

--- app.js
+++ app.js
@@ -24,10 +24,74 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+const searchForThreeLetterCombo = (string, combo) => {
+    let matches = [];
+    let restOfString = "";
+    for (let i = 0; i < string.length -1 ; i++) {
+        if (string.substring(i, i + 2) === combo) {
+            matches.push(i);
+            
+            restOfString = string.substring(i + 2);
+            console.log("found "+restOfString);
+            break; // Stop searching after the combo is found
+        }
+    }
+    return { matches, restOfString };
+};
+function extractBetweenCombos(string, startCombo, endCombo) {
+    let startIndex = string.indexOf(startCombo);
+    if (startIndex === -1) {
+        return ""; // Start combo not found
+    }
+
+    startIndex += startCombo.length; // Adjust start index to exclude startCombo
+    let endIndex = string.indexOf(endCombo, startIndex);
+    if (endIndex === -1) {
+        return ""; // End combo not found after start combo
+    }
+
+    //console.log("jo" + string.substring(startIndex, endIndex));
+    return string.substring(startIndex, endIndex);
+}
+
 app.post("/convert-mp3", async (req, res) => {
   //call to youtube api
-  const videoId = req.body.videoID ;
+  let videoId = req.body.videoID ;
   console.log(videoId);
+  let result = "";
+  let videosplice = " " ;
+  //youtube app
+  if(videoId&&videoId.includes(".be"))
+  {
+    console.log("Video ID contains '.be' so it is a youtube app request");
+    result = extractBetweenCombos(videoId,"e/","?s");
+    console.log("Substring extracted from video ID:", result);
+   // console.log("Result is" + result.toString());
+   // console.log("Matches:", result.matches);
+   // console.log("Rest of string:", result.restOfString);
+    videosplice = result.toString();
+    console.log("happens " + videosplice);
+    videoId = videosplice;
+    
+  }
+
+  //youtube page
+  else if(videoId&&videoId.includes("be."))
+  {
+    console.log("Video ID contains 'be.' so it is a youtube webpage request");
+    // has to be two charaters long
+    result = searchForThreeLetterCombo(videoId,"v=");
+    console.log("Substring extracted from video ID:", result);
+    console.log("Result is" + result.restOfString);
+   // console.log("Matches:", result.matches);
+    console.log("Rest of string:", result.restOfString);
+    videosplice = result.restOfString;
+    console.log("happens " + videosplice);
+    videoId = videosplice;
+    
+  }
+
+  //add functionailty to deipher if its a webpage url video link or a youtube video link
     if(
         videoId === undefined||
         videoId === ""||
